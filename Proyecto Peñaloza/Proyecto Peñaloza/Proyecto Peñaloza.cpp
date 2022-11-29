7df3407f3c23a04dcb96412ce41b0207d3aafc71
Bruno5000 <brunopicazo11@aragon.unam.mx>
2022-11-29 15:58:29 -0600

    Version 7, avanzamos con gran parte del codigo

--- Proyecto Peñaloza/Proyecto Peñaloza/Proyecto Peñaloza.cpp
+++ Proyecto Peñaloza/Proyecto Peñaloza/Proyecto Peñaloza.cpp
@@ -58,3 +58,56 @@ int main(int argc, char* argv[])//esta es la forma estandar de declarar la funci
 
 void	Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int iEstadisticas[], int& iNumElementos)
 {
+
+	//Sustituya estas lineas por su código
+	iNumElementos = 1;
+	strcpy(szPalabras[0], "AquiVaElDiccionario");
+	iEstadisticas[0] = 1; // la primer palabra aparece solo una vez.
+
+	FILE* archivo;
+
+	fopen_s(&archivo, szNombre, "r");//leer el archivo
+	if (archivo != NULL)//si el archivo si se pudo leer
+	{
+		char textoEnBruto[700];
+
+		//Leer todo el archivo hasta su final, guardando todo el texto del mismo en un arr|ay
+		while (!feof(archivo))
+		{
+			for (int i = 0; i != NUMPALABRAS; i++)
+			{
+				//fgets(texto[i], NUMPALABRAS, archivo);
+				fscanf(archivo, "%s", textoEnBruto[i]);
+				//printf("%s\n", texto[i]);
+			}
+			fclose(archivo);
+		}
+
+		int caracterszNombre, numpalabra = 0, j = 0;
+		char szpalabrasinorden[][TAMTOKEN]{ 'hola','pepe','C' };
+		char arrayAxiliar[TAMTOKEN];
+		struct PALABRA_CON_FREC
+		{
+			char laPalabra[TAMTOKEN];
+			int frecuencia = 1;
+		}unaPalabra;
+
+		//empezar a separar el array palabra por palabra en un array exclusivo para cada palabra
+		while (szNombre[caracterszNombre] != '\0')
+		{
+			caracterszNombre = 0;
+			//va a evitar tomar como valido cararcteres vacios y signos de puntuación
+			while (caracterszNombre != ' ' && caracterszNombre != '\0' && caracterszNombre != '	' && caracterszNombre != ',' && caracterszNombre != '.' && caracterszNombre != '(' && caracterszNombre != ')' && caracterszNombre != '¿' && caracterszNombre != '?' && caracterszNombre != '!' && caracterszNombre != '¡')
+			{
+				//en este caso lo que pasa es que toma el valor caracterszNombre y despues de usar el valor le suma 1 a el valor
+				szpalabrasinorden[numpalabra][caracterszNombre++] = szNombre[caracterszNombre];
+			}
+			//se salio de este ciclo porque encontro un carater que debe de evitar
+			caracterszNombre = '\0';
+			numpalabra++;
+			if (szNombre[caracterszNombre] != '\0')
+			{
+				/*esta linea es necesaria para hacer que recorra la siguiente casilla del
+				arreglo ya que si se deja en la misma casilla nuca más va entar al while
+				de arriba y solo va almacenar una sola palabra*/
+				caracterszNombre++;
